3df7c0f150c31f6afd231fd442fae37c27a9c6b6
Arjun <arjunsinghanya>
2026-07-01 09:47:52 +0530

    navbar mobile view dropdown functionality improvement

--- src/component/nav/navcenter/NavCenter.jsx
+++ src/component/nav/navcenter/NavCenter.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import './NavCenter.css';
 import { BiSolidDownArrow } from "react-icons/bi";
@@ -15,28 +15,46 @@ const services = [
 ];
 
 function NavCenter({ isOpen = false, onNavigate }) {
+  const [serviceOpen, setServiceOpen] = useState(false);
+
+  useEffect(() => {
+    if (!isOpen) {
+      setServiceOpen(false);
+    }
+  }, [isOpen]);
+
+  const handleNavigate = () => {
+    setServiceOpen(false);
+    onNavigate?.();
+  };
+
   return (
     <div className={`nav-center ${isOpen ? 'nav-center-open' : ''}`}>
-      <Link className="nav-link" to="/" onClick={onNavigate}>HOME</Link>
-      <Link className="nav-link" to="/about" onClick={onNavigate}>ABOUT US</Link>
+      <Link className="nav-link" to="/" onClick={handleNavigate}>HOME</Link>
+      <Link className="nav-link" to="/about" onClick={handleNavigate}>ABOUT US</Link>
 
-      <div className="nav-dropdown">
-        <button className="nav-link nav-dropdown-button" type="button">
+      <div className={`nav-dropdown ${serviceOpen ? 'nav-dropdown-open' : ''}`} onMouseLeave={() => setServiceOpen(false)}>
+        <button
+          className="nav-link nav-dropdown-button"
+          type="button"
+          aria-expanded={serviceOpen}
+          onClick={() => setServiceOpen((open) => !open)}
+        >
           SERVICES 
-          <BiSolidDownArrow/>
+          <BiSolidDownArrow className="nav-dropdown-icon" />
         </button>
         
 
         <div className="dropdown-menu">
           {services.map((service) => (
-            <Link className="dropdown-link" to={service.path} key={service.path} onClick={onNavigate}>
+            <Link className="dropdown-link" to={service.path} key={service.path} onClick={handleNavigate}>
               {service.label}
             </Link>
           ))}
         </div>
       </div>
 
-      <Link className="nav-link" to="/contact" onClick={onNavigate}>CONTACT US</Link>
+      <Link className="nav-link" to="/contact" onClick={handleNavigate}>CONTACT US</Link>
     </div>
   );
 }
